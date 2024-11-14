7267e67efb57f2b95dd58a015f388304496024f7
YagurtovAnton <Yagur_oo18@top-academy.ru>
2024-11-14 22:08:46 +0300

    Digit bauttons DONE

---
+++ Calk/resource.h
@@ -0,0 +1,29 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Resource.rc
+//
+#define IDI_ICON1                       101
+
+#define IDC_EDIT_DISPLAY				999
+
+#define IDC_BATTON_0				1000
+#define IDC_BATTON_1				1001
+#define IDC_BATTON_2				1002
+#define IDC_BATTON_3				1003
+#define IDC_BATTON_4				1004
+#define IDC_BATTON_5				1005
+#define IDC_BATTON_6				1006
+#define IDC_BATTON_7				1007
+#define IDC_BATTON_8				1008
+#define IDC_BATTON_9				1009
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
